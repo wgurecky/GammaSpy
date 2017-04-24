fixes jacobian calc in lin bg model

gammaspy/gammaData/bg.py
@@ -9,6 +9,7 @@ import numdifftools as nd
 
 class LinModel(object):
     """!
+    @brief Linear background model.
     """
     def __init__(self, init_params=[1., 1.]):
         self._params = init_params
@@ -27,34 +28,34 @@ class LinModel(object):
 
     def eval(self, params, x):
         """!
+        @brief Evaluate the linear background model
         """
         model_f = params[0] * x + params[1]
         return model_f
 
     def integral(self, a, b, params):
         """!
-        @brief Compute definite integral of general gaussian model.
+        @brief Compute definite integral of linear model.
         @param a Start.
         @param b End.
-        @param params  Gaussian model parameter array (len=3)
+        @param params  model parameter array (len=2)
         """
         f_f = params[0] * b ** 2. / 2. + params[1] * b
         f_i = params[0] * a ** 2. / 2. + params[1] * a
         return f_f - f_i
 
-    def int_hess(self, a, b, params):
+    def int_jac(self, a, b, params):
         """!
-        @brief Computes hessian of integral for
+        @brief Computes jacobian of integral for
         area uncertainty calculations.
         \f[
         H^-1 \approx C
         \f]
-        Where $C$ is the covar matrix and $H$ is the hessian.
+        Where $C$ is the covar matrix and $H$ is the jacobian.
         """
         reduced_int = lambda p: self.integral(a, b, p)
         jac = nd.Jacobian(reduced_int)(params)
-        h_approx = np.dot(jac.T, jac)
-        return h_approx
+        return jac
 
 
 def bg_model_factory(name, **kwargs):
@@ -73,4 +74,4 @@ def bg_model_factory(name, **kwargs):
 if __name__ == "__main__":
     lm = LinModel()
     print(lm.integral(0, 20., [0., 100.]))
-    print(lm.int_hess(0, 20., [0., 100.]))
+    print(lm.int_jac(0, 20., [0., 100.]))